476712afdf3e36e0f83d99803896204ecbfeeeb4
uploads <85631165+21Turtles@users.noreply.github.com>
2025-10-21 13:54:00 -0400

    Update memory.c

--- memory.c
+++ memory.c
@@ -5,12 +5,20 @@
 
 void main(void)
 {
-while (1) {
-int z = RA0
+int z = RA6;
 int a = (RC0 * z);
 int b = (RC1 * z);
 int c = (RC2 * z);
 int d = (RC3 * z);
+int x = 0;
+    
+int a0 = 0; int b0 = 0; int c0 = 0; int d0 = 0;
+int a1 = 0; int b1 = 0; int c1 = 0; int d1 = 0;
+int a2 = 0; int b2 = 0; int c2 = 0; int d2 = 0;
+int a3 = 0; int b3 = 0; int c3 = 0; int d3 = 0;
+    
+while (1) {
+
     }
         
 }
