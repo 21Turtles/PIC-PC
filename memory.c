7675acd29a400106185c56fc93c77a0b2ee2d2c4
uploads <85631165+21Turtles@users.noreply.github.com>
2025-10-21 13:54:47 -0400

    Update memory.c

--- memory.c
+++ memory.c
@@ -16,6 +16,10 @@ int a0 = 0; int b0 = 0; int c0 = 0; int d0 = 0;
 int a1 = 0; int b1 = 0; int c1 = 0; int d1 = 0;
 int a2 = 0; int b2 = 0; int c2 = 0; int d2 = 0;
 int a3 = 0; int b3 = 0; int c3 = 0; int d3 = 0;
+int a4 = 0; int b4 = 0; int c4 = 0; int d4 = 0;
+int a5 = 0; int b5 = 0; int c5 = 0; int d5 = 0;
+int a6 = 0; int b6 = 0; int c6 = 0; int d6 = 0;
+int a7 = 0; int b7 = 0; int c7 = 0; int d7 = 0;
     
 while (1) {
 
