9008e605814277841aae55805eac3013197333ac
uploads <85631165+21Turtles@users.noreply.github.com>
2025-10-24 11:23:57 -0400

    Update memory.c

--- memory.c
+++ memory.c
@@ -18,6 +18,13 @@ int a = RC4; //Input 1
 int b = RC5; //Input 2
 int c = RC6; //Input 3
 int d = RC7; //Input 4
+
+//Outputs is RC0,1,2,3- 4-bit
+
+int o0 = RC0; //Input 1
+int o1 = RC1; //Input 2
+int o2 = RC2; //Input 3
+int o3 = RC3; //Input 4
     
 int x = 0; //Read or Write
     
@@ -39,7 +46,56 @@ int a6 = 0; int b6 = 0; int c6 = 0; int d6 = 0; //byte 7 - Address id = 0110 6
 int a7 = 0; int b7 = 0; int c7 = 0; int d7 = 0; //byte 8 - Address id = 0111 7
     
 while (1) {
-    if (y3 == 0 && y2 == 0 && y1 == 0 && y0 == 0) {} else if (y3 == 0 && y2 == 0 && y1 == 0 && y0 == 0) {} else if (y3 == 0 && y2 == 0 && y1 == 0 && y0 == 0) {
+    if (y3 == 0 && y2 == 0 && y1 == 0 && y0 == 0) {
+        a0 = a;
+        b0 = b;
+        c0 = c;
+        d0 = d;
+    } else if (y3 == 0 && y2 == 0 && y1 == 0 && y0 == 1) {
+        a1 = a;
+        b1 = b;
+        c1 = c;
+        d1 = d;
+    } else if (y3 == 0 && y2 == 0 && y1 == 1 && y0 == 0) {
+        a2 = a;
+        b2 = b;
+        c2 = c;
+        d2 = d;
+    } else if (y3 == 0 && y2 == 0 && y1 == 1 && y0 == 1 {
+        a3 = a;
+        b3 = b;
+        c3 = c;
+        d3 = d;
+    } else if (y3 == 0 && y2 == 1 && y1 == 0 && y0 == 0 {
+        a4 = a;
+        b4 = b;
+        c4 = c;
+        d4 = d;
+    } else if (y3 == 0 && y2 == 1 && y1 == 0 && y0 == 1 {
+        a5 = a;
+        b5 = b;
+        c5 = c;
+        d5 = d;
+    } else if (y3 == 0 && y2 == 1 && y1 == 1 && y0 == 0 {
+        a6 = a;
+        b6 = b;
+        c6 = c;
+        d6 = d;
+    } else if (y3 == 0 && y2 == 1 && y1 == 1 && y0 == 1 {
+        a7 = a;
+        b7 = b;
+        c7 = c;
+        d7 = d;
+    }
+
+
+
+
+
+
+
+
+    
 
 
 
